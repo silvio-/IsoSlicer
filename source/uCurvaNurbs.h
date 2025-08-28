af27a6272edd89970d2ea40070dd8c45156ee70c
silvio- <sbm@cin.ufpe.br>
2025-08-28 17:40:36 -0300

    Update and rename uCurvaNurbs.h to source/uCurvaNurbs.h

--- source/uCurvaNurbs.h
+++ source/uCurvaNurbs.h
@@ -9,18 +9,18 @@
 class cnurbs{
 	public:
 	int n; //grau dos segmentos de curva
-	int K;  //número de nós sem multiplicidades
-	int Kr; //número de nós com multiplicidades
-	int L; //quantidade de intervalos úteis (sem multiplicidades)
-	int c; //número de pontos de controle: Vale: k+1=n+c
-	Ponto *d; //pontos de controle em coords homogêneas
-	Ponto *_d; //pontos de controle auxiliares em coords homogêneas
-	double *u; //vetor de nós da B-spline
-	int *uix; //vetor de índices de nós, considerando as multiplicidades
-	int *uuteis; //vetor dos índices dos intervalos de domínio (úteis) em uix, pelo ix do último múltiplo
-	int *r; // vetor de multiplicidade dos nós
+	int K;  //nÃºmero de nÃ³s sem multiplicidades
+	int Kr; //nÃºmero de nÃ³s com multiplicidades
+	int L; //quantidade de intervalos Ãºteis (sem multiplicidades)
+	int c; //nÃºmero de pontos de controle: Vale: k+1=n+c
+	Ponto *d; //pontos de controle em coords homogÃªneas
+	Ponto *_d; //pontos de controle auxiliares em coords homogÃªneas
+	double *u; //vetor de nÃ³s da B-spline
+	int *uix; //vetor de Ã­ndices de nÃ³s, considerando as multiplicidades
+	int *uuteis; //vetor dos Ã­ndices dos intervalos de domÃ­nio (Ãºteis) em uix, pelo ix do Ãºltimo mÃºltiplo
+	int *r; // vetor de multiplicidade dos nÃ³s
 	void inicialize(int grau, int k, int kr); // inicializa estruturas internas
-	void defina_intervalos_uteis(); // define os índices dos intervalos úteis
+	void defina_intervalos_uteis(); // define os Ã­ndices dos intervalos Ãºteis
 	void destrua(); // desaloca estruturas internas
 	void Pcarregue(int i, Ponto P);
 	void Ucarregue(int i, double U, int Mult);
